27c3815825a0d8c4176d77f7b337430903a68f18
Matheus-Grego <matheusgrego6@gmail.com>
2025-04-03 18:56:07 -0300

    tela-cadastrar-voo-funcionario

---
+++ frontend/src/app/voo/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import React, { useState } from "react";
+import { TextField, Button, Select, MenuItem, FormControl, InputLabel } from "@mui/material";
+
+const aeroportos = [
+    "Aeroporto Internacional de Guarulhos (GRU)",
+    "Aeroporto Internacional do Galeão (GIG)",
+    "Aeroporto Internacional de Brasília (BSB)",
+    "Aeroporto de Congonhas (CGH)",
+    "Aeroporto Santos Dumont (SDU)",
+    "Aeroporto Internacional do Recife (REC)",
+    "Aeroporto Internacional Salgado Filho (POA)",
+    "Aeroporto Internacional de Salvador (SSA)",
+    "Aeroporto Internacional de Curitiba (CWB)",
+    "Aeroporto Internacional de Fortaleza (FOR)",
+];
+
+
+
+const RegisterFly = () => {
+
+const [valor, setValor] = useState('');
+const [milhas, setMilhas] = useState(0);
+const handleChange = (event) => {
+    let inputValue = event.target.value;
+    inputValue = inputValue.replace(/[^0-9.]/g, ""); 
+
+    if (inputValue && parseFloat(inputValue) > 0) {
+        setValor(inputValue);
+        setMilhas(parseFloat((inputValue * 1.2).toFixed(3)));
+    } else {
+        setValor(""); 
+        setMilhas(0)
+    }
+};
+    return (
+        <div className="flex h-screen">
+            <div className="w-1/2 bg-black flex justify-center items-center">
+                <img className="w-full h-full object-cover object-top" src="registerfly.jpg" alt="Imagem de voo" />
+            </div>
+
+
+            <div className="w-1/2 flex justify-center items-center p-8 flex-col">
+                    <div className="text-[#FF3D00] text-3xl font-semibold">
+                        Cadastrar Voo
+                    </div>
+                <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
+                    <form className="flex flex-col gap-4">
+                        
+                        <TextField label="Código do Voo" variant="outlined" fullWidth required />
+                        <TextField label="Data" type="date" variant="outlined" fullWidth InputLabelProps={{ shrink: true }} required />
+
+                        <FormControl fullWidth>
+                            <InputLabel>Aeroporto de origem</InputLabel>
+                            <Select required>
+                                <MenuItem value="">Selecione o Aeroporto</MenuItem>
+                                {aeroportos.map((aeroporto, index) => (
+                                    <MenuItem key={index} value={aeroporto}>
+                                        {aeroporto}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+
+                        <FormControl fullWidth >
+                            <InputLabel>aeroporto de destino</InputLabel>
+                            <Select required>
+                                <MenuItem value="">Selecione</MenuItem>
+                                {aeroportos.map((aeroporto, index) => (
+                                    <MenuItem key={index} value={aeroporto}>
+                                        {aeroporto}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+
+                        
+                        <TextField
+                            label="Valor da passagem"
+                            type="text"
+                            variant="outlined"
+                            fullWidth
+                            required
+                            value={valor}
+                            onChange={handleChange}
+                        />
+                                {valor ? (
+                                         <span>ou {milhas} pontos em milhas</span>
+                                    ) : (<div></div>)}
+                       
+
+
+
+
+                       <Button 
+                            variant="contained" 
+                            className="bg-[#FF3D00] hover:bg-[#D63000]" 
+                            type="submit" 
+                            fullWidth
+                        >
+                            Registrar
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterFly;
