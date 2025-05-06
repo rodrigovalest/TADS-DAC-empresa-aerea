263b3f65deca1543416402bf4009b34f2d163568
Humanaice <matheusnsantana@gmail.com>
2025-05-06 16:03:55 -0300

    fix: Arruma pagina inicial Imagem nao estava carregando com a header sem padding

--- frontend/src/app/mainpage/homeheader.tsx
+++ frontend/src/app/mainpage/homeheader.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 import React, { useState } from "react";
-import '../../../public/styles/header.css';
+import "../../../public/styles/header.css";
 
 const HomeHeader = () => {
-
-    return(
-        <header className="header shadow-lg flex px-10 justify-between items-center text-[#D63000]">
-            <img width="70vh" src="/airtads-logo.png"></img>
-            <div className="flex gap-2">
-                <a href="/login">Entrar</a>
-                <a href="/autocadastro">Criar conta</a>
-            </div>
-            
-        </header>
-    )
-
-
-}
+  return (
+    <header className="header shadow-lg flex px-10 py-3 justify-between items-center text-[#D63000]">
+      <img width="70vh" src="/images/airtads-logo.png" alt="AirTads Logo" />
+      <div className="flex gap-2">
+        <a href="/login">Entrar</a>
+        <a href="/autocadastro">Criar conta</a>
+      </div>
+    </header>
+  );
+};
 
 export default HomeHeader;
