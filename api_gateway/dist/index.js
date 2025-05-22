9cc41fc70992a7fac7df9d71844dfb0e2246e0ee
Matheus-Grego <matheusgrego6@gmail.com>
2025-05-22 17:32:50 -0300

    fix: cors in api gateway

---
+++ api_gateway/dist/index.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.use((0, cors_1.default)());
+app.use((req, res, next) => {
+    console.log(`[${req.method}] ${req.originalUrl}`);
+    next();
+});
+const services = {
+    clientes: 'http://localhost:8080',
+    funcionarios: 'http://localhost:8081',
+    reservas: 'http://localhost:8082',
+    voos: 'http://localhost:8083',
+    auth: 'http://localhost:8084',
+};
+Object.entries(services).forEach(([route, target]) => {
+    app.use(`/${route}`, (0, http_proxy_middleware_1.createProxyMiddleware)({
+        target,
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/${route}`]: '',
+        },
+        logger: console,
+    }));
+});
+app.use((req, res) => {
+    res.status(404).json({ error: 'Rota não encontrada no API Gateway.' });
+});
+app.listen(port, () => {
+    console.log(`🛡️ API Gateway rodando em http://localhost:${port}`);
+});
