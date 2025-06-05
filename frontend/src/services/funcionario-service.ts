7e14d8d0b2a9ab6236eef9f38b76fb4647ee0181
Matheus Grego <137663305+Matheus-Grego@users.noreply.github.com>
2025-06-05 17:01:27 -0300

    Revert "add: rotas service funcionario"

--- frontend/src/services/funcionario-service.ts
+++ frontend/src/services/funcionario-service.ts
@@ -3,72 +3,25 @@ import IInserirFuncionarioRequest from "@/models/requests/inserir-funcionario-re
 import IFuncionarioResponse from "@/models/response/funcionario-response";
 import IInserirFuncionarioResponse from "@/models/response/inserir-funcionario-response";
 
-const API_GATEWAY_URL = "http://localhost:8000"; 
 const funcionarioService = {
   inserirFuncionario: async (data: IInserirFuncionarioRequest): Promise<IInserirFuncionarioResponse> => {
     throw new Error("Not implemented yet");
   },
 
-  findFuncionarioById: async (id: number): Promise<IFuncionarioResponse> => {
-    const response = await fetch(`${API_GATEWAY_URL}/funcionarios/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  
-    if (!response.ok) {
-      if (response.status === 404) {
-        throw new Error(`Funcionário com id ${id} não encontrado`);
-      }
-      throw new Error("Erro ao buscar funcionário");
-    }
-  
-    return await response.json();
+  findFuncionarioById: async (): Promise<IFuncionarioResponse> => {
+    throw new Error("Not implemented yet");
   },
 
-  atualizarFuncionario: async ( id: number, data: IAtualizarFuncionarioRequest): Promise<IFuncionarioResponse> => {
-    const response = await fetch(`${API_GATEWAY_URL}/funcionarios/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-  
-    if (!response.ok) {
-      throw new Error(`Erro ao atualizar funcionário com ID ${id}`);
-    }
-  
-    return await response.json();
+  atualizarFuncionario: async (data: IAtualizarFuncionarioRequest): Promise<IFuncionarioResponse> => {
+    throw new Error("Not implemented yet");
   },
 
   findAllFuncionarios: async (): Promise<IFuncionarioResponse []> => {
-    const response = await fetch(`${API_GATEWAY_URL}/funcionarios/listarFuncionarios`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  
-    if (!response.ok) {
-      throw new Error("Erro ao buscar funcionários");
-    }
-  
-    return await response.json();
+    throw new Error("Not implemented yet");
   },
 
-  removerFuncionario: async (id: number): Promise<void> => {
-    const response = await fetch(`${API_GATEWAY_URL}/funcionarios/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  
-    if (!response.ok) {
-      throw new Error(`Erro ao remover funcionário com ID ${id}`);
-    }
+  removerFuncionario: async (): Promise<void> => {
+    throw new Error("Not implemented yet");
   },
 };
 
